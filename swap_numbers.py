081f476e617cea6f9e0b876af2d74b7f0206b482
priyankakharat2817-cyber <priyankakharat2817@gmail.com>
2026-07-16 22:19:39 +0530

    Add number swapping functionality in swap_numbers.py

---
+++ swap_numbers.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before Swapping:")
+print("a =", a)
+print("b =", b)
+
+a, b = b, a
+
+print("After Swapping:")
+print("a =", a)
+print("b =", b)
